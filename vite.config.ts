80937bfa2949a9ea31afc8a597202ead7c29a981
Ashok Goli <ashok2ashok@gmail.com>
2025-09-15 13:24:55 -0400

    Update base path in vite.config.ts to default to root for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  base: process.env.NODE_ENV === 'production' ? '/web/' : '/', // GitHub Pages subdirectory deployment
+  base: '/',
   publicDir: 'public',
   build: {
     outDir: 'dist',
